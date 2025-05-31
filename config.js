f96ac96e89b7205cfc195f8c28a013777ce99ac8
Thinura-Nethz <thinuranethsara999@gmail.com>
2025-05-31 10:43:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined? "AzkwQAQA#ZtyYLO7IHQZI87tH2vRSPu6RCHH6dCaq7_cqyj3ruJI":process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined? "gncF0S4J#RozYOc8AYESD_8B7K0xYK9Z_E_-qR-CHvWd8Q9pJDfI":process.env.SESSION_ID,
 MONGODB: process.env.MONGODB || "mongodb://mongo:XLdvkJvrWYaLLrweahaKXtVwcTIWiJeu@caboose.proxy.rlwy.net:18642",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
